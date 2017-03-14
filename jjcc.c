bdb178c88f039f9ced309e02b8813d0830a6876a
hwq <15265036423@163.com>
2017-03-14 11:22:47 +0800

    First commit
    add some files for c.

---
+++ jjcc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int b = 8;
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    printf("hello world\n");
+    return 0;
+}
---
+++ jk.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a=1;
+    int b=3;
+    printf("%d\n",a+b);
+    return 0;
+}
